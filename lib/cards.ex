984304960a174941dcb7018674e31f921553902f
Alex VKO <ale@alexvko.com>
2017-01-07 12:09:22 -0200

    Adds documentation and tests to Cards module

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and dealing with deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of cards
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -18,10 +25,34 @@ defmodule Cards do
     Enum.shuffle(cards)
   end
 
+  @doc """
+  Determines whether a deck contains a given card
+
+  ## Examples
+
+      iex> Cards.create_deck |> Cards.contains?("Ace of Spades")
+      true
+
+      iex> Cards.create_deck |> Cards.contains?("blah")
+      false
+  """
   def contains?(cards, hand) do
     Enum.member?(cards, hand)
   end
 
+  @doc """
+    Devides a deck into a hand and the remainder of the deck
+    The `hand_size` argument indicates how many cards should be
+    in the hand.
+
+  ## Examples
+
+    iex> deck = Cards.create_deck
+    iex> {hand, _deck} = Cards.deal(deck, 1)
+    iex> hand
+    ["Ace of Spades"]
+
+  """
   def deal(cards, hand_size) do
     Enum.split(cards, hand_size)
   end
