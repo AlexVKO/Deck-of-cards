249b8c053b681ede0fb53d6c48b602604232d3bd
Alex VKO <ale@alexvko.com>
2017-01-06 22:56:16 -0200

    Created deck

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,11 @@
 defmodule Cards do
-  def hello do
-    "hi there!"
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
   end
 end
